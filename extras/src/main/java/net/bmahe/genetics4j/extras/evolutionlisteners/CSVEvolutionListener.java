257c23bda8f6408a9ab6061d78936be560f7b6a9
Bruno Mahé <bruno@bmahe.net>
2020-05-12 01:32:54 -0700

    Add Fitness Sharing on top of a new concept for Post Evaluation processing.
    This fix #21

--- extras/src/main/java/net/bmahe/genetics4j/extras/evolutionlisteners/CSVEvolutionListener.java
+++ extras/src/main/java/net/bmahe/genetics4j/extras/evolutionlisteners/CSVEvolutionListener.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
@@ -79,7 +80,8 @@ public abstract class CSVEvolutionListener<T extends Comparable<T>, U> implement
 			csvPrinter = openPrinter();
 		}
 
-		final U context = evolutionContextSupplier().apply(generation, population, fitness, isDone);
+		final Optional<U> context = Optional
+				.ofNullable(evolutionContextSupplier().apply(generation, population, fitness, isDone));
 
 		for (int individualIndex = 0; individualIndex < population.size(); individualIndex++) {
 			final int individualIndexFinal = individualIndex;
