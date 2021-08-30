677bc14faa7df6fab0df9fd5a029dc4c6e060243
Bruno Mahé <bruno@bmahe.net>
2021-08-29 17:00:44 -0700

    Add support for Multi Point Crossover for double

--- core/src/main/java/net/bmahe/genetics4j/core/combination/multipointcrossover/MultiPointCrossoverCombinationHandler.java
+++ core/src/main/java/net/bmahe/genetics4j/core/combination/multipointcrossover/MultiPointCrossoverCombinationHandler.java
@@ -9,6 +9,7 @@ import net.bmahe.genetics4j.core.combination.ChromosomeCombinatorHandler;
 import net.bmahe.genetics4j.core.combination.ChromosomeCombinatorResolver;
 import net.bmahe.genetics4j.core.spec.chromosome.BitChromosomeSpec;
 import net.bmahe.genetics4j.core.spec.chromosome.ChromosomeSpec;
+import net.bmahe.genetics4j.core.spec.chromosome.DoubleChromosomeSpec;
 import net.bmahe.genetics4j.core.spec.chromosome.IntChromosomeSpec;
 import net.bmahe.genetics4j.core.spec.combination.CombinationPolicy;
 import net.bmahe.genetics4j.core.spec.combination.MultiPointCrossover;
@@ -30,8 +31,8 @@ public class MultiPointCrossoverCombinationHandler implements ChromosomeCombinat
 		Validate.notNull(combinationPolicy);
 		Validate.notNull(chromosome);
 
-		return combinationPolicy instanceof MultiPointCrossover
-				&& (chromosome instanceof BitChromosomeSpec || chromosome instanceof IntChromosomeSpec);
+		return combinationPolicy instanceof MultiPointCrossover && (chromosome instanceof BitChromosomeSpec
+				|| chromosome instanceof IntChromosomeSpec || chromosome instanceof DoubleChromosomeSpec);
 	}
 
 	@Override
@@ -48,6 +49,10 @@ public class MultiPointCrossoverCombinationHandler implements ChromosomeCombinat
 
 		if (chromosome instanceof IntChromosomeSpec) {
 			return new IntChromosomeMultiPointCrossover(random, (MultiPointCrossover) combinationPolicy);
+		}
+
+		if (chromosome instanceof DoubleChromosomeSpec) {
+			return new DoubleChromosomeMultiPointCrossover(random, (MultiPointCrossover) combinationPolicy);
 		}
 
 		throw new IllegalArgumentException("Could not handle chromosome " + chromosome);
