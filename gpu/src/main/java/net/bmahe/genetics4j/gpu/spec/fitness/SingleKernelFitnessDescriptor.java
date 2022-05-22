246f4fe49cdf5c46d64389aa8e6b3111ab079423
Bruno Mahé <bruno@bmahe.net>
2022-05-22 16:29:17 -0700

    Add some javadoc

--- gpu/src/main/java/net/bmahe/genetics4j/gpu/spec/fitness/SingleKernelFitnessDescriptor.java
+++ gpu/src/main/java/net/bmahe/genetics4j/gpu/spec/fitness/SingleKernelFitnessDescriptor.java
@@ -10,19 +10,55 @@ import net.bmahe.genetics4j.gpu.spec.fitness.cldata.ResultAllocator;
 import net.bmahe.genetics4j.gpu.spec.fitness.cldata.StaticDataLoader;
 import net.bmahe.genetics4j.gpu.spec.fitness.kernelcontext.KernelExecutionContextComputer;
 
+/**
+ * Describes all the necessary information to execute an OpenCL kernel
+ * <p>
+ * As we may execute across multiple GPUs, may of these are computed at runtime
+ * and may vary from GPU to GPU as their specs may differ
+ */
 @Value.Immutable
 public interface SingleKernelFitnessDescriptor {
 
+	/**
+	 * Name of the kernel to execute
+	 * 
+	 * @return
+	 */
 	String kernelName();
 
+	/**
+	 * Computer for the kernel execution context (ex: globak work size)
+	 * 
+	 * @return
+	 */
 	KernelExecutionContextComputer kernelExecutionContextComputer();
 
+	/**
+	 * Association of kernel argument index and a static data loader
+	 * 
+	 * @return
+	 */
 	Map<Integer, StaticDataLoader> staticDataLoaders();
 
+	/**
+	 * Association of kernel argument index and a data loader
+	 * 
+	 * @return
+	 */
 	Map<Integer, DataLoader> dataLoaders();
 
+	/**
+	 * Association of kernel argument index and a local memory allocator
+	 * 
+	 * @return
+	 */
 	Map<Integer, LocalMemoryAllocator> localMemoryAllocators();
 
+	/**
+	 * Association of kernel argument index and a result allocator
+	 * 
+	 * @return
+	 */
 	Map<Integer, ResultAllocator> resultAllocators();
 
 	static class Builder extends ImmutableSingleKernelFitnessDescriptor.Builder {
--- gpu/src/main/java/net/bmahe/genetics4j/gpu/spec/fitness/multistage/StageDescriptor.java
+++ gpu/src/main/java/net/bmahe/genetics4j/gpu/spec/fitness/multistage/StageDescriptor.java
@@ -9,23 +9,67 @@ import net.bmahe.genetics4j.gpu.spec.fitness.cldata.LocalMemoryAllocator;
 import net.bmahe.genetics4j.gpu.spec.fitness.cldata.ResultAllocator;
 import net.bmahe.genetics4j.gpu.spec.fitness.kernelcontext.KernelExecutionContextComputer;
 
+/**
+ * Fully describes how to execute a specific stage with OpenCL
+ *
+ */
 @Value.Immutable
 public interface StageDescriptor {
 
+	/**
+	 * Kernel name
+	 * 
+	 * @return
+	 */
 	String kernelName();
 
+	/**
+	 * Computer for the kernel execution context (ex: globak work size)
+	 * 
+	 * @return
+	 */
 	KernelExecutionContextComputer kernelExecutionContextComputer();
 
+	/**
+	 * Association of kernel argument index and a data loader
+	 * 
+	 * @return
+	 */
 	Map<Integer, DataLoader> dataLoaders();
 
+	/**
+	 * Association of kernel argument index and a local memory allocator
+	 * 
+	 * @return
+	 */
 	Map<Integer, LocalMemoryAllocator> localMemoryAllocators();
 
+	/**
+	 * Association of kernel argument index and a result allocator
+	 * 
+	 * @return
+	 */
 	Map<Integer, ResultAllocator> resultAllocators();
 
+	/**
+	 * Association of the kernel argument used for a result of the previous stage to
+	 * the kernel argument for this execution
+	 * 
+	 * @return
+	 */
 	Map<Integer, Integer> reusePreviousResultAsArguments();
 
+	/**
+	 * Association of the size of the data from the result of the previous stage to
+	 * the kernel argument for this execution
+	 * 
+	 * @return
+	 */
 	Map<Integer, Integer> reusePreviousResultSizeAsArguments();
 
+	/**
+	 * Association of static data to a kernel argument index
+	 */
 	Map<String, Integer> mapStaticDataAsArgument();
 
 	static class Builder extends ImmutableStageDescriptor.Builder {
