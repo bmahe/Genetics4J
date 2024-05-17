5aa9dec341de018e80badc265db3cab5201347e4
Bruno Mahé <bruno@bmahe.net>
2024-05-17 00:32:19 -0700

    Use Objects::requireNonNull for bitChromosomeUtils

--- core/src/main/java/net/bmahe/genetics4j/core/util/BitChromosomeUtils.java
+++ core/src/main/java/net/bmahe/genetics4j/core/util/BitChromosomeUtils.java
@@ -1,6 +1,7 @@
 package net.bmahe.genetics4j.core.util;
 
 import java.util.BitSet;
+import java.util.Objects;
 
 import org.apache.commons.lang3.Validate;
 
@@ -9,8 +10,8 @@ import net.bmahe.genetics4j.core.chromosomes.BitChromosome;
 public class BitChromosomeUtils {
 
 	public static int hammingDistance(final BitChromosome bc1, final BitChromosome bc2) {
-		Validate.notNull(bc1);
-		Validate.notNull(bc2);
+		Objects.requireNonNull(bc1);
+		Objects.requireNonNull(bc2);
 		Validate.isTrue(bc1.getNumAlleles() == bc2.getNumAlleles());
 
 		final BitSet bitSet1 = bc1.getBitSet();
